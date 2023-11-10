443dd10ee2792c166e0f5ea583c8bda438f1c202
Suchith M <suchith.m@surya-digital.com>
2023-11-10 10:30:27 +0530

    initial setup

---
+++ android/app/src/main/java/suchith/m/MainActivity.java
@@ -0,0 +1,5 @@
+package suchith.m;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'suchith.m',
+  appName: 'suchith-m',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
